9445728aee984cc018a27caccb084777067f5cd5
harry123180 <harry123180@gmail.com>
2022-11-13 03:00:49 +0800

    changeMix

--- Servo_test/Servo_test.ino
+++ Servo_test/Servo_test.ino
@@ -1,22 +1,27 @@
-#include <Servo.h>
+#include"ServoTimer2.h"
+#include <TimerOne.h>
 // 右輪停止界線1538 
 // 左輪停止界線1536
-Servo left;
-Servo right;
+ServoTimer2 left;
+ServoTimer2 right;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   left.attach(11);
   right.attach(13);
+   Timer1.initialize(100000);
+  Timer1.attachInterrupt(PID); 
 
 }
-
+void PID(){
+  
+}
 void loop() {
   // put your main code here, to run repeatedly:
-  int k = 0;
-  left.writeMicroseconds(1536+k);
+  int k = 30;
+  left.write(1536+60);
   //delay(1000);
-  right.writeMicroseconds(1538-k);
+  right.write(1538-k);
 
 
 
--- mix/mix.ino
+++ mix/mix.ino
@@ -1,17 +1,26 @@
 //int 整數 //float 小數點(浮點數) char 字元 str 字串 void空的 bool 布林 變數型態
-#include <Servo.h>
-#include <PID_v1.h>
+#include"ServoTimer2.h"
+//#include <PID_v1.h>
+#include <TimerOne.h>
 #include "Wire.h"
 #include "sensorbar.h"
 int trigPin = 7;    // 超音波感測器 Trig腳接 Arduino pin 6
 int echoPin = 6;    //超音波感測器 Echo 腳接 Arduino pin 7
 long duration,cm ;
-
+float error;
+float de=0;//微分項誤差
+float ie=0;//積分項誤差
+float error_befor[5]={0,0,0,0,0};//前項誤差
+float dt =0.1; //單次執行離散週期
+float kp=5;
+float ki=1.6;
+float kd=1.8;
+float fix_value =0;//修正量
 char cmd[9] ;
-Servo servo1;
-Servo servo2;
+ServoTimer2 servo1;
+ServoTimer2 servo2;
 bool ArrayStatus[8];
-int8_t rawValue;
+uint8_t rawValue;
 //int k ;
 const uint8_t SX1509_ADDRESS = 0x3E;  // SX1509 I2C address (00)
 int speed=30;
@@ -21,63 +30,44 @@ void readLine() {
   delay(10);
 }
 void forward() { //前
-  
-  servo1.writeMicroseconds(1536+speed);
-  servo2.writeMicroseconds(1538-speed);
-}
-void L1() { 
-  servo1.writeMicroseconds(1536+speed);
-  servo2.writeMicroseconds(1538-speed-20);
-}
-void L2() {  
-  servo1.writeMicroseconds(1536+speed);
-  servo2.writeMicroseconds(1538-speed-40);
-}
-void L3() {  
-  servo1.writeMicroseconds(1536+speed);
-  servo2.writeMicroseconds(1538-speed-50);
-}
-void L4() {  
-  servo1.writeMicroseconds(1536+speed);
-  servo2.writeMicroseconds(1538-speed-60);
-}
-void L5() {  
-  servo1.writeMicroseconds(1536+speed);
-  servo2.writeMicroseconds(1538-speed-70);
-}
-void L6() {  
-  servo1.writeMicroseconds(1536+speed);
-  servo2.writeMicroseconds(1538-speed-90);
-}
-void R1() {  //右
-  servo1.writeMicroseconds(1536+speed+20);
-  servo2.writeMicroseconds(1538-speed);
-}
-void R2() {  //慢右
-  servo1.writeMicroseconds(1536+speed+40);
-  servo2.writeMicroseconds(1538-speed);
-}
-void R3() {
-  servo1.writeMicroseconds(1536+speed+50);
-  servo2.writeMicroseconds(1538-speed);
-}
-void R4() {
-  servo1.writeMicroseconds(1536+speed+60);
-  servo2.writeMicroseconds(1538-speed);
-}
-void R5() {
-  servo1.writeMicroseconds(1536+speed+70);
-  servo2.writeMicroseconds(1538-speed);
-}
-void R6() {
-  servo1.writeMicroseconds(1536+speed+90);
-  servo2.writeMicroseconds(1538-speed);
-}
-void stop1(){
-  servo1.writeMicroseconds(1536);
-  servo2.writeMicroseconds(1538);
+  servo1.write(1536+speed);
+  servo2.write(1538-speed);
 }
 
+void stop1(){
+  servo1.write(1536);
+  servo2.write(1538);
+}
+void fix(float v){
+  if(v<-10 && v>-60){
+    servo1.write(1536+speed-int(v));
+    servo2.write(1538-speed-int(v));
+    Serial.print("向右");
+  }
+  else if (v>10 && v< 60){
+    servo1.write(1536+speed-int(v));
+    servo2.write(1538-speed-int(v));
+    Serial.print("向左");
+  }
+  else if(v<10 || v<-10){
+    forward();
+    Serial.print("do forward()");
+  }
+  /*
+  else if(v>60){
+    servo1.write(1536+speed-int(v));
+    servo2.write(1538-50);
+    Serial.print("向右");    
+  }
+  else if(v<-60){
+    servo1.write(1536+50);
+    servo2.write(1538-speed-int(v));
+    Serial.print("向左");
+   
+  }
+   */
+  Serial.println(fix_value);
+}
 void setup() {
   servo1.attach(11);
   servo2.attach(13);
@@ -88,68 +78,56 @@ void setup() {
   pinMode(echoPin, INPUT);       //超音波被障礙物反射後，Arduino讀取感測器Echo腳的時間差
   uint8_t returnStatus = mySensorBar.begin();
   mySensorBar.clearInvertBits();
-  mySensorBar.setInvertBits();
+  Timer1.initialize(100000);
+  Timer1.attachInterrupt(PID); 
 }
 void loop(){
-  
   readLine();//讀線狀態，回傳8個位元的值
-//  Serial.println(rawValue);
-  rawValue=mySensorBar.getPosition();
-
-  Serial.println(rawValue);
-  switch(rawValue){
-  case 0:
-    forward();
-    Serial.println("0");
-    break;
-  case 4:
-    L1();
-    break;
-  case 15:
-    L2();
-    break;
-  case 9:
-    L3();
-    break;
-  case 26:
-    L4();
-    break;
-  case 13:
-    L5();
-    break;
-  case 37:
-    L6();
-    break;
-  case -4:
-    R1();
-    break;
-  case -15:
-    R2();
-    break;
-  case -9:
-    R3();
-    break;
-  case -26:
-    R4();
-    break;
-  case -13:
-    R5();
-    break;
-  case -37:
-    R6();
-    break;
-  default:
-    stop1();
-    break;
+  //Serial.println(rawValue);
+  rawValue=mySensorBar.getRaw();//.getPosition();
+  
+}
+void PID(){
+  if(rawValue!=0&& rawValue!=255){
+  error = log(rawValue)/log(2)-4.58;
+  calculation(error);
+  fix(fix_value);
   }
-/*
-  digitalWrite(trigPin, LOW); //程式計算出距離值 cm
-  delayMicroseconds(1000);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(1000);
-  digitalWrite(trigPin, LOW);
-  pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
-  cm = (duration/2) / 29.1;  
-*/
+  else if(rawValue ==0 || rawValue ==255){
+    stop1();
+  }
+}
+float buffer_right_move(float *buffer , int buf_len)
+{
+	int i ;
+	char tmp = buffer[buf_len - 1];
+	for(i = buf_len ; i > 0 ; i--)
+	{
+		buffer[i] = buffer[i-1] ; 
+	}
+	buffer[0] = error ;
+}
+
+void calculation(float cerror){
+  ie = ie+(error*dt);
+  /*
+  for(int j=0;j<5;j++){
+    if(j<4)
+    de = de+((error_befor[j]-error_befor[j+1])/dt);
+    else
+    de = de+(error_befor[j]-error_befor[0])/dt;
+    de = de;
+  }
+  */
+  de = (cerror-error_befor[0])/dt;
+  error_befor[0]=cerror;
+  //buffer_right_move(error_befor,5) ; 
+
+  fix_value = kp*cerror+ki*ie+kd*de;
+  /*
+  if(fix_value>70)
+  fix_value=70;
+  else if(fix_value<-70)
+  fix_value=-70;
+  */
 }
